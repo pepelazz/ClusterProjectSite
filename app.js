4cbd2b69f68369b5c688a46e3d46397d4550e374
pepelazz <pepelazz00@gmail.com>
2015-01-26 22:21:46 +0400

    1

--- app.js
+++ app.js
@@ -20,10 +20,11 @@ $(function() {
 
 
 },{}],"/Users/Trikster/static_sites/ClusterProject/_ClusterProject/src/javascript/util.coffee":[function(require,module,exports){
-var fixSize;
+var fixSize, navHideAnimation;
 
 $(function() {
   fixSize();
+  navHideAnimation();
   return $(window).on('resize', (function() {
     fixSize();
   }));
@@ -40,6 +41,43 @@ fixSize = (function() {
       height: $('.smoke-picture').offset().top - (logoTextEl.offset().top + logoTextEl.innerHeight())
     });
   }
+});
+
+navHideAnimation = (function() {
+  var $document, $element, $window, dHeight, elHeight, elTop, wHeight, wScrollBefore, wScrollCurrent, wScrollDiff;
+  $element = $('.nav');
+  if (!$element.length) {
+    return true;
+  }
+  elHeight = 0;
+  elTop = 0;
+  $document = $(document);
+  dHeight = 0;
+  $window = $(window);
+  wHeight = 0;
+  wScrollCurrent = 0;
+  wScrollBefore = 0;
+  wScrollDiff = 0;
+  $window.on("scroll", function() {
+    elHeight = $element.outerHeight();
+    dHeight = $document.height();
+    wHeight = $window.height();
+    wScrollCurrent = $window.scrollTop();
+    wScrollDiff = wScrollBefore - wScrollCurrent;
+    elTop = parseInt($element.css("top")) + wScrollDiff;
+    if (wScrollCurrent <= 0) {
+      $element.css("top", 0);
+    } else if (wScrollDiff > 0) {
+      $element.css("top", (elTop > 0 ? 0 : elTop));
+    } else if (wScrollDiff < 0) {
+      if (wScrollCurrent + wHeight >= dHeight - elHeight) {
+        $element.css("top", ((elTop = wScrollCurrent + wHeight - dHeight) < 0 ? elTop : 0));
+      } else {
+        $element.css("top", (Math.abs(elTop) > elHeight ? -elHeight : elTop));
+      }
+    }
+    wScrollBefore = wScrollCurrent;
+  });
 });
 
 
