25393aeefce3ae39db797558fcce99486b3f3a3e
pepelazz <pepelazz00@gmail.com>
2015-02-04 16:21:27 +0400

    1

--- app.js
+++ app.js
@@ -1,11 +1,32 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({"./src/javascript/app.coffee":[function(require,module,exports){
 require('./util');
 
+require('./articleMenu');
+
 require('./loader');
 
 
 
-},{"./loader":"/Users/Trikster/static_sites/ClusterProject/_ClusterProject/src/javascript/loader.coffee","./util":"/Users/Trikster/static_sites/ClusterProject/_ClusterProject/src/javascript/util.coffee"}],"/Users/Trikster/static_sites/ClusterProject/_ClusterProject/src/javascript/loader.coffee":[function(require,module,exports){
+},{"./articleMenu":"/Users/Trikster/static_sites/ClusterProject/_ClusterProject/src/javascript/articleMenu.coffee","./loader":"/Users/Trikster/static_sites/ClusterProject/_ClusterProject/src/javascript/loader.coffee","./util":"/Users/Trikster/static_sites/ClusterProject/_ClusterProject/src/javascript/util.coffee"}],"/Users/Trikster/static_sites/ClusterProject/_ClusterProject/src/javascript/articleMenu.coffee":[function(require,module,exports){
+$(function() {
+  var $menu, headerHeight;
+  $menu = $('.article-menu');
+  if ($menu.length === 0) {
+    return;
+  }
+  headerHeight = $('.article-header-bg').offset().top + $('.article-header-bg').innerHeight();
+  return $(window).on("scroll", function() {
+    if ($(window).scrollTop() > headerHeight) {
+      return $menu.addClass('fixed');
+    } else {
+      return $menu.removeClass('fixed');
+    }
+  });
+});
+
+
+
+},{}],"/Users/Trikster/static_sites/ClusterProject/_ClusterProject/src/javascript/loader.coffee":[function(require,module,exports){
 $(function() {
   $('#hideAll .loader').css({
     marginTop: window.innerHeight * 0.4
@@ -19,29 +40,8 @@ $(function() {
 
 
 
-},{}],"/Users/Trikster/static_sites/ClusterProject/_ClusterProject/src/javascript/util.coffee":[function(require,module,exports){
-var fixSize, navHideAnimation;
-
-$(function() {
-  fixSize();
-  navHideAnimation();
-  return $(window).on('resize', (function() {
-    fixSize();
-  }));
-});
-
-fixSize = (function() {
-  var logoTextEl;
-  if ($('#first-page-banner').length > 0) {
-    $('#first-page-banner').css({
-      height: window.innerHeight
-    });
-    logoTextEl = $('.logo-text', '#first-page-banner');
-    $('.rocket-picture').css({
-      height: $('.smoke-picture').offset().top - (logoTextEl.offset().top + logoTextEl.innerHeight())
-    });
-  }
-});
+},{}],"/Users/Trikster/static_sites/ClusterProject/_ClusterProject/src/javascript/navHide.coffee":[function(require,module,exports){
+var navHideAnimation;
 
 navHideAnimation = (function() {
   var $document, $element, $window, dHeight, elHeight, elTop, wHeight, wScrollBefore, wScrollCurrent, wScrollDiff;
@@ -80,6 +80,36 @@ navHideAnimation = (function() {
   });
 });
 
+module.exports = navHideAnimation;
 
 
-},{}]},{},["./src/javascript/app.coffee"]);
+
+},{}],"/Users/Trikster/static_sites/ClusterProject/_ClusterProject/src/javascript/util.coffee":[function(require,module,exports){
+var fixSize, navHideAnimation;
+
+navHideAnimation = require('./navHide');
+
+$(function() {
+  fixSize();
+  navHideAnimation();
+  return $(window).on('resize', (function() {
+    fixSize();
+  }));
+});
+
+fixSize = (function() {
+  var logoTextEl;
+  if ($('#first-page-banner').length > 0) {
+    $('#first-page-banner').css({
+      height: window.innerHeight
+    });
+    logoTextEl = $('.logo-text', '#first-page-banner');
+    $('.rocket-picture').css({
+      height: $('.smoke-picture').offset().top - (logoTextEl.offset().top + logoTextEl.innerHeight())
+    });
+  }
+});
+
+
+
+},{"./navHide":"/Users/Trikster/static_sites/ClusterProject/_ClusterProject/src/javascript/navHide.coffee"}]},{},["./src/javascript/app.coffee"]);
